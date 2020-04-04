aa1a56270e8c454b1ca89cc60f316ef02af75a56
Ralph Minderhoud <ralphminderhoud@gmail.com>
2020-04-04 01:25:59 -0500

    Add 0.13.0 docs

---
+++ 0.13.0/implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["magick_rust"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"magick_rust/struct.HSL.html\" title=\"struct magick_rust::HSL\">HSL</a>","synthetic":false,"types":["magick_rust::wand::pixel::HSL"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ 0.13.0/source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["libc"] = {"name":"","dirs":[{"name":"windows","files":["mod.rs","msvc.rs"]}],"files":["fixed_width_ints.rs","lib.rs","macros.rs"]};
+sourcesIndex["magick_rust"] = {"name":"","dirs":[{"name":"wand","files":["drawing.rs","macros.rs","magick.rs","mod.rs","pixel.rs"]}],"files":["conversions.rs","lib.rs"]};
+createSourceSidebar();
